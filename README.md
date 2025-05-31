2e5ffe07e381ca222c48153624916585237fa238
David <60181640+kaden-mk@users.noreply.github.com>
2025-05-31 02:42:06 +0200

    3d model loading with tiny obj

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Vulkan Renderer Class Test
 This is not meant for public use, but if for whatever reason you want to use it just go for it. This is just meant for me to see how well I could learn vulkan and use it in my other projects thats all, the only reason its public to get opinions from other people
+
+# Current To-do
+* Optimize vertices to only include:
+* positions as x, y, z numbers
+* colors as unsigned 8 bits
+* uvs as unsigned 8 bits
+
+* batching (store filepath in dict and return the already created model and then just use 1 draw call for each mesh)
+* see how to render an entire scene with one draw call (if thats even possible)
--- VkRenderer/headers/VulkanModel.hpp
+++ VkRenderer/headers/VulkanModel.hpp
@@ -6,6 +6,7 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 
+#include <memory>
 #include <vector>
 
 namespace VkRenderer {
@@ -15,14 +16,22 @@ namespace VkRenderer {
 			struct Vertex {
 				glm::vec3 position;
 				glm::vec3 color;
+				glm::vec3 normal;
+				glm::vec2 uv;
 
 				static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 				static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+				bool operator==(const Vertex& other) const {
+					return position == other.position && color == other.color && normal == other.normal && uv == other.uv;
+				}
 			};
 
 			struct Builder {
 				std::vector<Vertex> vertices{};
 				std::vector<uint32_t> indices{};
+
+				void loadModel(const std::string& filepath);
 			};
 
 			VulkanModel(VulkanDevice& device, const VulkanModel::Builder &builder);
@@ -30,6 +39,8 @@ namespace VkRenderer {
 
 			VulkanModel(const VulkanModel&) = delete;
 			VulkanModel& operator=(const VulkanModel&) = delete;
+
+			static std::unique_ptr<VulkanModel> createModelFromFile(VulkanDevice& device, const std::string& filepath);
 
 			void bind(VkCommandBuffer commandBuffer);
 			void draw(VkCommandBuffer commandBuffer);
