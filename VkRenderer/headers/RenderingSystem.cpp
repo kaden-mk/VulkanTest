d6d3dd145a1f86a270893e639a212ae475f2bd63
David <60181640+kaden-mk@users.noreply.github.com>
2025-06-02 00:52:21 +0200

    better shader organization

--- VkRenderer/headers/RenderingSystem.cpp
+++ VkRenderer/headers/RenderingSystem.cpp
@@ -50,7 +50,7 @@ namespace VkRenderer {
 		pipelineConfig.renderPass = renderPass;
 		pipelineConfig.pipelineLayout = pipelineLayout;
 
-		pipeline = std::make_unique<VulkanPipeline>(device, "VkRenderer/shaders/vert.spv", "VkRenderer/shaders/frag.spv", pipelineConfig);
+		pipeline = std::make_unique<VulkanPipeline>(device, "VkRenderer/shaders/compiled/vert.spv", "VkRenderer/shaders/compiled/frag.spv", pipelineConfig);
 	}
 
 	void RenderingSystem::renderObjects(FrameInfo& frameInfo)
--- VkRenderer/shaders/compile.bat
+++ VkRenderer/shaders/compile.bat
@@ -1,3 +1,5 @@
-E:\VulkanSDK\1.4.304.0\Bin\glslc.exe shader.vert -o vert.spv
-E:\VulkanSDK\1.4.304.0\Bin\glslc.exe shader.frag -o frag.spv
-pause
+@echo off
+cd /d "%~dp0"
+
+E:\VulkanSDK\1.4.304.0\Bin\glslc.exe shader.vert -o compiled\vert.spv
+E:\VulkanSDK\1.4.304.0\Bin\glslc.exe shader.frag -o compiled\frag.spv
